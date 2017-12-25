118acbf326b44ba64092d110d4ca70e66465e492
eemmresen <eemmresen@gmail.com>
2017-12-25 18:29:52 +0200

    Add files via upload

---
+++ Adres takip sistemi 2/src/bp2proje/adres.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bp2proje;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author sunay
+ */
+public class adres {
+public String il,ilce,mahalle,sokak;
+public int no;
+int ıd;
+//    ArrayList<adres> iller = new ArrayList<>();
+//    ArrayList<adres> ilceler = new ArrayList<>();
+//    ArrayList<adres> mahalleler = new ArrayList<>();
+//    ArrayList<adres> sokaklar = new ArrayList<>();
+//    ArrayList<adres> nolar = new ArrayList<>();
+    
+     
+}
